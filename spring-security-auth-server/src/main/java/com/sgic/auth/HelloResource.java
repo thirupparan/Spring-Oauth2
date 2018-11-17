0b8450007895a4526ade923a1c303ca76c6a738c
Thirupparan <thirupparan1994@gmail.com>
2018-11-17 09:38:52 +0530

    Oauth2 two server added

---
+++ spring-security-auth-server/src/main/java/com/sgic/auth/HelloResource.java
@@ -0,0 +1,22 @@
+package com.sgic.auth;
+
+import java.security.Principal;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/rest/hello")
+public class HelloResource {
+	@GetMapping("/principal")
+	public Principal user(Principal principal) {
+		return principal;
+	}
+
+	@GetMapping
+	public String hello() {
+		return "Hello World";
+	}
+
+}
